ecc52cc578d8ce27b139abe482f8633de3a30287
Shayan <mnik414@gmail.com>
2021-04-14 23:05:00 +0430

    creat login

--- nextjs/src/components/register/Register.jsx
+++ nextjs/src/components/register/Register.jsx
@@ -4,19 +4,21 @@ export default function Login() {
   return (
     <div className={styles.login}>
       <div className={styles.loginWrapper}>
-        <div className={styles.loginTop}>
+        <div className={styles.loginLeft}>
           <h3 className={styles.loginLogo}>NEXTOHS</h3>
           <span className={styles.loginDesc}>
             برای ورود نام کاربری و رمز خود را وارد کنید
           </span>
         </div>
-        <div className={styles.loginBottom}>
+        <div className={styles.loginRight}>
           <div className={styles.loginBox}>
             <input placeholder="نام کاربری" className={styles.loginInput} />
             <input placeholder="رمز عبور" className={styles.loginInput} />
             <button className={styles.loginButton}>ورود</button>
             <span className={styles.loginForget}>بازیابی رمز عبور؟</span>
-            <button className={styles.loginRegisterButton}>ثبت نام</button>
+            <button className={styles.loginRegisterButton}>
+              ساخت اکانت جدید
+            </button>
           </div>
         </div>
       </div>
